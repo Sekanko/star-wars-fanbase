c21bfd4899a5df1e58492ba1091ac359332d9ab3
Oskar Tarka <oskar0208@gmail.com>
2025-01-11 19:18:28 +0100

    ---

---
+++ fanbase-data/src/main/java/pl/edu/pjwstk/fanbasedata/FanbaseDataApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.fanbasedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanbaseDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanbaseDataApplication.class, args);
+    }
+
+}
---
+++ fanbase-web-api/src/main/java/pl/edu/pjwstk/fanbasewebapi/FanbaseWebApiApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.fanbasewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanbaseWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanbaseWebApiApplication.class, args);
+    }
+
+}
